82a51e4765ed958f44084ee9d5ba408c14841e4b
Loreina Chew <loreina.chew@gmail.com>
2020-05-05 12:49:49 -0400

    feat: add layout and linting

---
+++ components/Layout.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+const Layout = styled.div`
+  max-width: 36rem;
+  padding: 0 2rem;
+  margin: 6rem auto 6rem;
+`
+
+export default Layout
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+import Layout from '../components/Layout'
+
+function Home() {
+  return <Layout>Loreina Chew</Layout>
+}
+
+export default Home
