b701186275e8bb9e1c3be82f60db5f74d2572be3
loreina <loreina98@gmail.com>
2021-01-13 12:12:04 -0500

    setup next and tailwind

--- components/index.jsx
+++
@@ -1,7 +0,0 @@
-import Chip from './Chip'
-import Header from './Header'
-import Layout from './Layout'
-import Menu from './Menu'
-import Socials from './Socials'
-
-export { Chip, Header, Layout, Menu, Socials }
--- styles/shared.jsx
+++
@@ -1,7 +0,0 @@
-import A from './Elements/A'
-import H1 from './Elements/H1'
-import H2 from './Elements/H2'
-import H3 from './Elements/H3'
-import Text from './Elements/Text'
-
-export { A, H1, H2, H3, Text }
