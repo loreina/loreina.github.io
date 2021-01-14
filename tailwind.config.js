3505c36acef4c06d0358ce8da2aaa9289bf2ff18
loreina <loreina98@gmail.com>
2021-01-13 21:49:23 -0500

    build header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      gray: colors.coolGray,
+    },
+    extend: {
+      colors: {
+        malibu: {
+          50: '#F1F6FF',
+          100: '#DAE7FE',
+          200: '#B5D0FD',
+          300: '#92B9FD',
+          400: '#6DA2FC',
+          500: '#5D8AD6',
+          600: '#5279BD',
+          700: '#416197',
+          800: '#36517E',
+          900: '#2C4165',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
