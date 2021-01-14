679f4cf5b34ec2631cb12f6272f366f01bac8c4c
loreina <loreina98@gmail.com>
2021-01-14 03:28:13 -0500

    setup tailwind for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx}'],
+  purge: ['./components/**/*.{js,jsx}', './pages/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
